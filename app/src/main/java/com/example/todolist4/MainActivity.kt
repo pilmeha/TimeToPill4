bb3e755a4049aa18ab473c14af7e1f20fd15e9dc
Ayur <garmayev.ayur@inbox.ru>
2024-04-17 13:04:20 +0500

    second path, 50%

--- app/src/main/java/com/example/todolist4/MainActivity.kt
+++ app/src/main/java/com/example/todolist4/MainActivity.kt
@@ -20,16 +20,10 @@ class MainActivity : AppCompatActivity()
         setContentView(binding.root)
         taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
         binding.newTaskButton.setOnClickListener{
-            NewTaskSheet().show(supportFragmentManager, "newTaskTag")
+            NewTaskSheet(null).show(supportFragmentManager, "newTaskTag")
         }
 
-        taskViewModel.name.observe(this){
-            binding.taskName.text = String.format("Task Name: %s", it)
-        }
 
-        taskViewModel.desc.observe(this){
-            binding.taskDesc.text = String.format("Task Desc: %s", it)
-        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
